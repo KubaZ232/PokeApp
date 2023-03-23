ca0c30521b0b7f811c4d47a495db5402d80bd4db
Your Name <jakub232489@gmail.com>
2023-03-23 18:28:14 +0100

    Intro component finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { createPortal } from 'react-dom';
 import "./App.css";
 import FlamesComponent from "./components/FlamesComponent";
 import MainPage from "./Pages/MainPage";
-import { motion, useAnimationControls } from "framer-motion"
 
 
 // dOdałem motion div i najpierw powinno się wyświetlić main page a później tamten komponent
 function App() {
+const [intro, setIntro] = useState(true);
+  useEffect( () => {
+    let introTimeout = setTimeout(
+      () => {  setIntro(false) }
+    , 9000)
+    return () => {
+      clearTimeout(introTimeout);
+      console.log("cleared timer");
+    }
+  },[]);
+
   return (
-    <motion.div>
-      <MainPage />
-      {/* <FlamesComponent/> */}
-    </motion.div>
+    <>
+      
+      {intro? <FlamesComponent/> : <MainPage /> }
+
+    </>
   );
 }
 
--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -2,26 +2,27 @@ import React,{useEffect, useState} from 'react'
 import { motion, useAnimationControls } from "framer-motion"
 
 const variants = {
-  open: { opacity: 1, y:0 },
-  closed: { opacity: 0, y: "-3%" }
+  closed: { opacity: 0, y: "-3%" },
+  open: { opacity: 1, y:0 } 
 }
 
 const MainPage = () => {
-  const [ isOpen, setIsOpen ] = useState(false);
+  // const [ isOpen, setIsOpen ] = useState(false);
   // Control for specifying when animation should start
   const controls = useAnimationControls();
-  useEffect(() => {
-    let mainPagePhotoTimer = setTimeout(
-      () => {
-        setIsOpen(true)
-        console.log("start timer");
-      }
-    , 4500)
-    return () => {
-      clearTimeout(mainPagePhotoTimer);
-      console.log("cleared timer");
-    }
-  },[]);
+
+  // useEffect(() => {
+  //   let mainPagePhotoTimer = setTimeout(
+  //     () => {
+  //       setIsOpen(true)
+  //       console.log("start timer");
+  //     }
+  //   , 4500)
+  //   return () => {
+  //     clearTimeout(mainPagePhotoTimer);
+  //     console.log("cleared timer");
+  //   }
+  // },[]);
   
 
   return (
@@ -30,23 +31,23 @@ const MainPage = () => {
       
     >
       <motion.div 
-        className="flex"
-        animate={isOpen ? "open" : "closed"}
-        variants={variants}
+        className="flex justify-center items-center"
+        // animate={isOpen ? "open" : "closed"}
+        // variants={variants}
       >
-        <div className="flex-auto w-32 ">01</div>
-        <div className="relative flex-auto w-80 ">
+  
+        <div className=" m-auto ">
           
             <img
               src="../public/poke1.jpg"
-              className="flex-auto object-center "
+              className="flex-auto object-center max-h-[42rem]"
             />
             
               {/* <input className='absolute p-2  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'/> */}
-              <p className="text-white text-center cursor-pointer" onClick={() => setIsOpen(isOpen => !isOpen)}>&#169; Jakub Zaręba </p>
+              {/* <p className="text-white text-center cursor-pointer" onClick={() => setIsOpen(isOpen => !isOpen)}>&#169; Jakub Zaręba </p> */}
         </div>
         
-        <div className="flex-auto w-32 ">03</div>  
+      
       </motion.div>
       
     </div>
